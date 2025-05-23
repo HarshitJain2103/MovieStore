261513543c1d7f27b702abaa776ebcf18b498cb1
HarshitJain2103 <163723613+HarshitJain2103@users.noreply.github.com>
2025-05-23 21:11:55 +0530

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./", // 👈 this allows @/components/... to resolve properly
+          },
+        },
+      ],
+    ],
+  };
+};
